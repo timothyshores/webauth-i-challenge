14bc85624818d3ffd0600653056d8844e8aa5fbc
Timothy Shores <timothymshores@gmail.com>
2019-05-06 15:02:47 -0500

    Installed jshint dev dependency. Create users/users-model.js with functinos add, find, findBy and findById. Import users-model into index.js.

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
+const Users = require('./users/users-model.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.post('/api/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 12);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
