61453dde1cfc0bb7aea946d7d07a841abf30329e
Timothy Shores <timothymshores@gmail.com>
2019-05-07 14:05:39 -0500

    Refactor auth/middleware.js to check for user's cookie rather than username and password in req.headers

--- auth/middleware.js
+++ auth/middleware.js
@@ -2,23 +2,9 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 function validUsernamePassword(req, res, next) {
-    const { username, password } = req.headers;
-
-    if (username && password) {
-        Users.findBy({ username })
-            .first()
-            .then(user => {
-                (user && bcrypt.compareSync(password, user.password))
-                    ? next()
-                    : res.status(401).json({ message: 'You shall not pass!' });
-            })
-            .catch(error => {
-                res.status(500).json(error);
-            });
-    }
-    else {
-        res.status(400).json({ message: 'You shall not pass!' })
-    }
+    (req.session && req.session.username)
+        ? next()
+        : res.status(401).json({ message: 'Please login to continue' });
 }
 
 module.exports = validUsernamePassword;
